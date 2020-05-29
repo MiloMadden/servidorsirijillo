a8284d25c4f60c2f817eea4922cd42ed77f9cdd1
MiloMadden <milogotze@outlook.com>
2020-05-29 16:31:40 -0500

    primer commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.json({
+        msn: 'Hello World!!!!!'
+    })
+  })
+   
+app.listen(3000, ()=>{
+    console.log('corriendo');
+    //console.log(process.env);
+})
